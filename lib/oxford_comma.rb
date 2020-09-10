305a9b3cb7530d105dbcb3368f87d249c0046b12
Dasha Lary <dk2800@nyu.edu>
2020-09-10 17:50:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,3 @@
-
-
 def oxford_comma(array)
 if array.length == 1
 return array.join
@@ -8,8 +6,5 @@ return array.join(" and ")
 elsif array.length > 2
 array[-1].insert(0, "and ")
 return array.join(", ")
-# elsif array.length > 3
-# return string1.insert(-2, " and ")
-
 end
 end
